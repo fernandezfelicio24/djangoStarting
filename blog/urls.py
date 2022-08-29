d2acc5e629b97eac785f8eda55c69adbe243a50e
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-08-29 15:09:28 +0700

    Learning Django #22 - Exercise Querysets to Views

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('soccer/', views.soccer),
     path('story/', views.story),
     path('news/', views.news),
     path('recent/', views.recent),
--- blog/views.py
+++ blog/views.py
@@ -18,6 +18,7 @@ def index(request):
         'nav': [
             ['/', 'Home'],
             ['/about', 'About'],
+            ['/blog/soccer', 'Soccer'],
             ['/blog/story', 'Story'],
             ['/blog/news', 'News'],
         ]
@@ -25,6 +26,25 @@ def index(request):
     #return HttpResponse('<h1>Ini adalah recent post</h1>')
     return render(request, 'blog/index.html', context)
 
+def soccer(request):
+    # QUERY SET
+    posts = Post.objects.filter(title='Soccer')
+    # print(posts)
+
+    context = {
+        'title_bar': 'Soccer',
+        'title_page': 'Everything about soccer',
+        'kontributor': 'vtl_it_reinaldo',
+        'app_css': 'blog/css/styles.css',
+        'Posts': posts,
+        'nav': [
+            ['/', 'Home'],
+            ['/about', 'About'],
+
+        ]
+    }
+
+    return render(request, 'blog/soccer.html', context)
 
 def story(request):
     context = {
