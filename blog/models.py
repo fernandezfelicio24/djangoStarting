f870eb034a5f1785626ff6e5563440123b88c1f7
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-09-03 09:58:36 +0700

    Practicing session #25 of learning Django

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 # class Post will be the name of table in database
 
@@ -9,9 +9,12 @@ class Post(models.Model):
     email = models.EmailField(default='defaultuser@gmail.com')
     adress = models.CharField(max_length=200, blank=True)
     category = models.CharField(max_length=255, default='Science')
-    slug = models.SlugField(default='football-team')
+    slug = models.SlugField(blank=True, editable=False)
     time_post = models.DateTimeField(auto_now_add=True)
 
+    def save(self):
+        self.slug = slugify(self.title)
+        super(Post, self).save()
 
     def __str__(self):
         return "{} . {}".format(self.id, self.title)
