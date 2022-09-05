78b520a244db212012948f010e2cc2292f19a6f3
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-09-05 07:39:01 +0700

    Just complete the session #24

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+admin.site.register(Post, PostAdmin)
