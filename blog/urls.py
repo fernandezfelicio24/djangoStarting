8962ca7f44d9091fe54dbb8225cb69a538b83214
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-09-12 07:38:30 +0700

    Session #28 exercising URL Name and Namespace

--- blog/urls.py
+++ blog/urls.py
@@ -7,14 +7,14 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('<str:categoryInput>/', views.categoryPost),
     path('post/<slug:slugInput>/', views.singlePost),
-    path('detail-post/<slug:slugInput>/', views.detailPost),
+    path('detail-post/<slug:slugInput>/', views.detailPost, name="detail-post"),
     path('category/<str:categoryInput>/', views.categoryPostingan, name="category"),
     #path('<int:id>/', views.handle_id),
     path('soccer/', views.soccer),
     #path('buisnes/', views.buisnes),
-    path('news/', views.news),
-    path('jurnal/', views.jurnal),
-    path('gossip/', views.gossip),
+    # path('news/', views.news),
+    # path('jurnal/', views.jurnal),
+    # path('gossip/', views.gossip),
 
 ]
 
