8ff8219b3fe6734ed474e2ddf7e98798a0c78fbb
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-09-21 08:35:55 +0700

    Learning Django session #36 - Model Form

--- blog/forms.py
+++ blog/forms.py
@@ -1,59 +1,85 @@
 from django import forms
 from django.template.defaultfilters import title
 
+#import model from models.py
+from .models import Post
 
-class PostForm(forms.Form):
+# class PostForm(forms.Form):
+#
+#     title = forms.CharField(
+#         label='Title',
+#         max_length=50,
+#         widget=forms.TextInput(
+#             attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Enter the title'
+#             }
+#         )
+#     )
+#
+#     body = forms.CharField(required=False,
+#              max_length=1000,
+#              widget=forms.Textarea(
+#              attrs={
+#                     'class': 'form-control',
+#                     }
+#             )
+#     )
+#
+#     email = forms.EmailField(
+#         label='Email Address',
+#         widget=forms.TextInput(
+#             attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Enter your valid email',
+#             }
+#         )
+#     )
+#
+#     address = forms.CharField(required=False,
+#             max_length=100,
+#             widget=forms.Textarea(
+#                 attrs={
+#                     'class': 'form-control',
+#                 }
+#             )
+#     )
+#
+#     category = forms.CharField(
+#             label='Category',
+#             max_length=30,
+#             widget=forms.TextInput(
+#                 attrs={
+#                     'class': 'form-control',
+#                     'placeholder': 'Category'
+#                 }
+#             )
+#         )
 
-    title = forms.CharField(
-        label='Title',
-        max_length=50,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter the title'
-            }
-        )
-    )
 
-    body = forms.CharField(required=False,
-             max_length=1000,
-             widget=forms.Textarea(
-             attrs={
-                    'class': 'form-control',
-                    }
-            )
-    )
 
-    email = forms.EmailField(
-        label='Email Address',
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter your valid email',
-            }
-        )
-    )
 
-    address = forms.CharField(required=False,
-            max_length=100,
-            widget=forms.Textarea(
-                attrs={
-                    'class': 'form-control',
-                }
-            )
-    )
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'body',
+            'email',
+            'adress',
+            'category'
+        ]
 
-    category = forms.CharField(
-            label='Category',
-            max_length=30,
-            widget=forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Category'
-                }
-            )
-        )
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter the title...', }),
+            'body' : forms.Textarea(attrs={'class': 'form-control', }),
+            'email' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your valid email', }),
+            'adress': forms.Textarea(attrs={'class': 'form-control',}),
+            'category' : forms.TextInput(attrs={'class': 'form-control','placeholder':'Category'}),
 
+        }
+        # https: // stackoverflow.com / questions / 12097825 / specifying - widget -
+        # for -model - form - extra - field - django
 
     def clean_title(self):
 
